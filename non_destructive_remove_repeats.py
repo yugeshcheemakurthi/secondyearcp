cf6680b78b61c125353e7c8c261c998637e3e3bf
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:01 +0530

    Initialize nondestructiveRemoveRepeats_python

---
+++ non_destructive_remove_repeats.py
@@ -0,0 +1,18 @@
+# nondestructiveRemoveRepeats(L)
+# Write the function nondestructiveRemoveRepeats(L), which takes a list L and nondestructively returns a new list in which any repeating elements in L are removed.
+# Specify the time complexity based on the solution that you have given.
+
+# Here are the sample test cases.
+# For example:
+# assert(nondestructiveRemoveRepeats([1, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 3, 5, 2, 7])
+# assert(nondestructiveRemoveRepeats([1, 5, 3, 3, 2, 1, 7, 5]) == [1, 5, 3, 2, 7])
+# assert(nondestructiveRemoveRepeats([1, 2, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 2, 3, 5, 7])
+
+def nondestructiveRemoveRepeats(L):
+    # Your code goes here...
+    pass
+
+assert(nondestructiveRemoveRepeats([1, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 3, 5, 2, 7])
+assert(nondestructiveRemoveRepeats([1, 5, 3, 3, 2, 1, 7, 5]) == [1, 5, 3, 2, 7])
+assert(nondestructiveRemoveRepeats([1, 2, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 2, 3, 5, 7])
+print ("All test cases passed....")
