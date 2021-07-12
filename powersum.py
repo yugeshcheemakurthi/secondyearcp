3057304216d3df6284421b61419a1c8f44fa072d
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:19 +0530

    Initialize powerSum_python

---
+++ powersum.py
@@ -0,0 +1,19 @@
+# powerSum(n, k) [15 pts]
+# Write the function powerSum(n, k) that takes two 
+# possibly-negative integers n and k and returns the 
+# so-called power sum: Specify the time complexity based 
+# on the solution that you have given. [Hint: This can be 
+# solved in (n * log k)  or better.
+
+#    1**k + 2**k + ... + n**k
+
+# If n is negative, return 0. Similarly, if k is negative, 
+# return 0.
+
+def powerSum(n, k):
+    # Your code goes here...
+    return 0
+
+# Write your own test cases here...
+
+print ("All test cases passed...")
