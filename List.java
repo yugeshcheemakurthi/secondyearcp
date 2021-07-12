81332f8a877feaf3c7564ae60fd1a2e06f9ba61e
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:15 +0530

    Initialize removeRowAndCol_java

---
+++ List.java
@@ -0,0 +1,34 @@
+// removeRowAndCol
+// Here we will write removeRowAndCol(row, col), 
+// Do not use copy.deepcopy and directly construct the 
+// modified 2d list.
+
+// The function takes a rectangular list L and two ints, 
+// row and col. the goal is to obtain a version of the list that 
+// has the given row and given column removed. You may assume that 
+// row and col are both legal values (that is, they are 
+// non-negative integers that are smaller than the largest 
+// row and column, respectively). For example, the list shown 
+// to the left would lead to the result shown on the right
+//  when called with row 1 and column 2.
+
+// list
+// [ [ 2, 3, 4, 5],
+//   [ 8, 7, 6, 5],
+//   [ 0, 1, 2, 3] ]
+
+// result
+// [ [ 2, 3, 5],
+//   [ 0, 1, 3] ]
+
+public class List {
+    public int[][] removeRowAndCol(int[][] arr, int row, int col) {
+        // Your code goes here...
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // Write your own test cases
+    }
+}
+
