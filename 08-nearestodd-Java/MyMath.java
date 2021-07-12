2a5769718feaff7295bfd04c35e9dc4090a981d7
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:00:50 +0530

    Initialize 08-nearestodd-Java

---
+++ 08-nearestodd-Java/MyMath.java
@@ -0,0 +1,11 @@
+// # Write the function nearestOdd(n) that takes an double n, 
+// # and returns as an int value the nearest odd number to n. 
+// # In the case of a tie, return the smaller odd value. 
+// # Note that the result must be an int, so nearestOdd(13.0) is the int 13, and not the double 13.0.
+
+public class MyMath {
+	public int nearestOdd(double n){
+		// your code goes here
+			return 0;
+	}
+}
