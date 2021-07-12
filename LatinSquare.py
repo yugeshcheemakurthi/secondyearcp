d666c0caa03513aedeba82eb02785f6c820c61d6
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:11 +0530

    Initialize isLatinSquare_python

---
+++ LatinSquare.py
@@ -0,0 +1,12 @@
+# isLatinSquare(a)
+# Write the function isLatinSquare(a) that takes a 2d list 
+# and returns True if it is a Latin square and False otherwise.
+
+# Check for Latin square in the following link:
+# https://en.wikipedia.org/wiki/Latin_square
+
+# Write your own test cases...
+
+def isLatinSquare(lst):
+    # Your code goes here...
+    pass
