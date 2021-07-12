e6e98531ffa6b73fb195304bd1799cb4f377365f
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:18 +0530

    Initialize collapseWhitespace_java

---
+++ MyString.java
@@ -0,0 +1,21 @@
+// Collapsewhitespaces [10 pts]
+// Without using the s.replace() method, write the function 
+// collapseWhitespace(s), that takes a string s and returns an 
+// equivalent string except that each occurrence of whitespace in 
+// the string is replaced by a single space. So, for example, 
+// collapseWhitespace("a\t\t\tb\n\nc") replaces the three tabs 
+// with a single space, and the two newlines with another single 
+// space , returning "a b c ". Here are a few more test cases for 
+// you:
+
+// assert(cw("a\nb") == "a b")
+// assert(cw("a\n   \t    b") == "a b")
+// assert(cw("a\n   \t    b  \n\n  \t\t\t c   ") == "a b c ")
+// These test cases are in python and please convert to java 
+// and execute them.
+// Once again, do not use s.replace() in your solution. 
+// You should not use a regular expression library.
+
+public class MyString {
+    // Your code goes here...
+}
