8e4a868cc20631c70384a34c70ff7a6b13b3c9e3
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:00:40 +0530

    Initialize 07-isfactor-Java

---
+++ 07-isfactor-Java/MyMath.java
@@ -0,0 +1,12 @@
+// # Write the function isFactor(f, n) that takes 
+// # two int values f and n, and returns True 
+// # if f is a factor of n, and False otherwise. 
+// # Note that every integer is a factor of 0.
+
+class MyMath {
+	public boolean isFactor(int f, int n){
+		// your code goes here
+
+		return false;
+	}
+}
