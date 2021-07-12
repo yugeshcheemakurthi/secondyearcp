59813020463c75f5c7333eddef290c335088958b
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:00:40 +0530

    Initialize 06-eggcartons-Java

---
+++ 06-eggcartons-Java/EggCartons.java
@@ -0,0 +1,11 @@
+// # Write the function eggCartons(eggs) that takes 
+// # a non-negative integer number of eggs, and returns 
+// # the smallest integer number of cartons required to hold 
+// # that many eggs, where a carton may hold up to 12 eggs
+
+class EggCartons {
+	public int eggCartons(int eggs) {
+		// Your code goes here....
+		return 0;
+	}
+}
