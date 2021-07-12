170828c610033b7f2c592387d03f05bb1323e59a
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:00:36 +0530

    Initialize 01-isrectangular-Java

---
+++ 01-isrectangular-Java/IsRectangular.java
@@ -0,0 +1,10 @@
+// # Write the function isRectangular(int[][] arr) that takes a possibly-2d 
+// # array arr and returns True  if it is rectangular, so each row has
+// #  the same number of elements. Return False otherwise.
+
+class IsRectangular {
+	public boolean isRectangular(int[][] arr) {
+		// Your code goes here...
+		return false;
+	}
+}
