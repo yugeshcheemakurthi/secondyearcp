d003f926b2dcdec9c9c087f914cbe111bb4d3ade
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:11 +0530

    Initialize isLatinSquare_java

---
+++ LatinSquare.java
@@ -0,0 +1,16 @@
+// isLatinSquare(a)
+// Write the function isLatinSquare(a) that takes a 2d list 
+// and returns True if it is a Latin square and False otherwise.
+
+// Check for Latin square in the following link:
+// https://en.wikipedia.org/wiki/Latin_square
+
+// Write your own test cases...
+
+public class LatinSquare {
+    public boolean isLatinSquare(int[][] arr) {
+        // Your code goes here...
+        
+        return false;
+    }
+}
