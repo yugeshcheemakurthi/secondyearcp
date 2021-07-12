7624749a1567a02f7cb5cb52059104170f7cf5ba
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:49 +0530

    Initialize nth_tenly_prime_python

---
+++ nth_tenly_prime.py
@@ -0,0 +1,21 @@
+#  We will say that a number is "tenly" (a made-up term) 
+#  if the digits of the number add up to 10.
+#  So 1153 is tenly, but 153 is not. With this in mind, 
+#  write the function nthTenlyPrime(n) that takes
+#  a non-negative int n and returns the nth number that 
+#  is both tenly and prime.
+#  You should also write all the required helper functions.
+#  The first several tenly primes are: 19, 37, 73, 109, 127
+
+def nthTenlyPrime(n):
+    # Your code goes here...
+    pass
+
+
+# Write your own test cases based on the above given test cases.
+assert(nthTenlyPrime(1) == 19)
+assert(nthTenlyPrime(2) == 37)
+assert(nthTenlyPrime(3) == 73)
+assert(nthTenlyPrime(4) == 109)
+assert(nthTenlyPrime(5) == 127)
+print ("All test cases passed...!")
