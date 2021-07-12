2e64b4b6edc41f714ddfe609c633d0fa9144c097
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:00:35 +0530

    Initialize 01-isrectangular-Python

---
+++ 01-isrectangular-Python/isrectangular.py
@@ -0,0 +1,11 @@
+# Write the function isrectangular(L) that takes a possibly-2d 
+# list L and returns True  if it is rectangular, so each row has
+#  the same number of elements. Return False otherwise.
+
+
+def fun_isrectangular(l):
+	# Your code goes here...
+	return False
+
+
+
