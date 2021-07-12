c5d7047023b91d576d82eb175875e47d20798ef7
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:15 +0530

    Initialize areAnagrams_str_python

---
+++ areAnagrams.py
@@ -0,0 +1,17 @@
+# areAnagrams(s1, s2)
+# Write the function areAnagrams(s1, s2) that takes two strings, 
+# s1 and s2, that you may assume contain only upper and/or 
+# lower case letters, and returns True if the strings are 
+# anagrams, and False otherwise. Two strings are anagrams 
+# if each can be reordered into the other. Treat "a" and "A"
+# as the same letters (so "Aba" and "BAA" are anagrams). 
+# You may not use sort() or sorted() or any other list-based
+# functions or approaches. Hint: you may use s.count(), 
+# which could be quite handy here.
+# Hint: The time complexity can be achieved in Linear.
+
+def areAnagrams(s1, s2):
+    # Your code goes here...
+    pass
+
+# write your test cases here...
