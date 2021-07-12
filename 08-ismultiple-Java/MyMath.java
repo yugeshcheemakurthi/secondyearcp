b72cbac9fa22f331e2394236620c51993d70e9bf
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:00:41 +0530

    Initialize 08-ismultiple-Java

---
+++ 08-ismultiple-Java/MyMath.java
@@ -0,0 +1,12 @@
+// # Write the function isMultiple that takes two int values m and n 
+// # and returns True if m is a multiple of n and False otherwise. 
+// # Note that 0 is a multiple of every integer including itself. 
+// # Also, you should make constructive use of the isFactor function you just wrote above.
+
+
+class MyMath {
+	public boolean isMultiple(int m, int n){
+		// Your code goes here...
+		return false;
+	}
+}
