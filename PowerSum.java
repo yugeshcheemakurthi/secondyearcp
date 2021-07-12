27e931e197a2271267a077c8296355c5516f1dcd
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:48 +0530

    Initialize powersum_java

---
+++ PowerSum.java
@@ -0,0 +1,19 @@
+// powerSum(n, k) [15 pts]
+// Write the function powerSum(n, k) that takes two 
+// possibly-negative integers n and k and returns the so-called 
+// power sum: Specify the time complexity based on the solution 
+// that you have given. [Hint: This can be solved in (n * log k)  
+// or better.
+
+//    1**k + 2**k + ... + n**k
+
+// If n is negative, return 0. Similarly, if k is negative, 
+// return 0.
+
+public class PowerSum {
+    // Your code goes here...
+
+    public static void main(String[] args) {
+        // Write your own test cases...
+    }
+}
