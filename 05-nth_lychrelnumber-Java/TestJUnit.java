c879ced60a61d466866cf5607085e5167171c873
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:47 +0530

    Initialize 05-nth_lychrelnumber-Java

---
+++ 05-nth_lychrelnumber-Java/TestJUnit.java
@@ -0,0 +1,64 @@
+/**
+ * This is JUnit that tests the method that is available in NthLychrelNumber class.
+ * This contains 1 testcases.
+ * 
+ * Please don't run this file.
+ * You can add your own test cases to this file by just copy and paste the last three 
+ * lines of the code (TestCase4).
+ * 
+ * @author: Siva Sankar
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestJUnit {
+   @Test
+   public void testCase1() {
+		assertEquals("1.", 196, new NthLychrelNumber().nthLychrelNumber(1));
+		assertEquals("2.", 295, new NthLychrelNumber().nthLychrelNumber(2));
+		assertEquals("3.", 394, new NthLychrelNumber().nthLychrelNumber(3));
+		assertEquals("4.", 493, new NthLychrelNumber().nthLychrelNumber(4));
+		assertEquals("5.", 592, new NthLychrelNumber().nthLychrelNumber(5));
+		assertEquals("6.", 689, new NthLychrelNumber().nthLychrelNumber(6));
+		assertEquals("7.", 691, new NthLychrelNumber().nthLychrelNumber(7));
+		assertEquals("8.", 788, new NthLychrelNumber().nthLychrelNumber(8));
+		assertEquals("9.", 790, new NthLychrelNumber().nthLychrelNumber(9));
+		assertEquals("10.", 879, new NthLychrelNumber().nthLychrelNumber(10));
+		assertEquals("11.", 887, new NthLychrelNumber().nthLychrelNumber(11));
+		assertEquals("12.", 978, new NthLychrelNumber().nthLychrelNumber(12));
+		assertEquals("13.", 986, new NthLychrelNumber().nthLychrelNumber(13));
+		assertEquals("14.", 1495, new NthLychrelNumber().nthLychrelNumber(14));
+		assertEquals("15", 1497, new NthLychrelNumber().nthLychrelNumber(15));
+		assertEquals("16.", 1585, new NthLychrelNumber().nthLychrelNumber(16));
+		assertEquals("17", 1587, new NthLychrelNumber().nthLychrelNumber(17));
+		assertEquals("18", 1675, new NthLychrelNumber().nthLychrelNumber(18));
+		assertEquals("19", 1677, new NthLychrelNumber().nthLychrelNumber(19));
+		assertEquals("20.", 1765, new NthLychrelNumber().nthLychrelNumber(20));
+		assertEquals("21.", 1767, new NthLychrelNumber().nthLychrelNumber(21));
+		assertEquals("22.", 1855, new NthLychrelNumber().nthLychrelNumber(22));
+		assertEquals("23.", 1857, new NthLychrelNumber().nthLychrelNumber(23));
+		assertEquals("24.", 1945, new NthLychrelNumber().nthLychrelNumber(24));
+		assertEquals("25.", 1947, new NthLychrelNumber().nthLychrelNumber(25));
+		assertEquals("26.", 1997, new NthLychrelNumber().nthLychrelNumber(26));
+		assertEquals("27.", 2494, new NthLychrelNumber().nthLychrelNumber(27));
+		assertEquals("28.", 2496, new NthLychrelNumber().nthLychrelNumber(28));
+		assertEquals("29.", 2584, new NthLychrelNumber().nthLychrelNumber(29));
+		assertEquals("30.", 2586, new NthLychrelNumber().nthLychrelNumber(30));
+		assertEquals("31.", 2674, new NthLychrelNumber().nthLychrelNumber(31));
+		assertEquals("32.", 2676, new NthLychrelNumber().nthLychrelNumber(32));
+		assertEquals("33.", 2764, new NthLychrelNumber().nthLychrelNumber(33));
+		assertEquals("34.", 2766, new NthLychrelNumber().nthLychrelNumber(34));
+		assertEquals("35.", 2854, new NthLychrelNumber().nthLychrelNumber(35));
+		assertEquals("36.", 2856, new NthLychrelNumber().nthLychrelNumber(36));
+		assertEquals("37.", 2944, new NthLychrelNumber().nthLychrelNumber(37));
+		assertEquals("38.", 2946, new NthLychrelNumber().nthLychrelNumber(38));
+		assertEquals("39.", 2996, new NthLychrelNumber().nthLychrelNumber(39));
+		assertEquals("40.", 3493, new NthLychrelNumber().nthLychrelNumber(40));
+		assertEquals("41.", 3495, new NthLychrelNumber().nthLychrelNumber(41));
+		assertEquals("42.", 3583, new NthLychrelNumber().nthLychrelNumber(42));
+		assertEquals("43.", 3585, new NthLychrelNumber().nthLychrelNumber(43));
+		assertEquals("44.", 3673, new NthLychrelNumber().nthLychrelNumber(44));
+		assertEquals("45.", 3675, new NthLychrelNumber().nthLychrelNumber(45));
+   }
+}
