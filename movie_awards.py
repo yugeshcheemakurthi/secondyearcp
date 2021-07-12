0efbc5d08290eb47257bf24728d4bfdd3bb4348d
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:05 +0530

    Initialize movieAwards_python

---
+++ movie_awards.py
@@ -0,0 +1,21 @@
+# Write the function movieAwards(oscarResults) that takes a set of tuples, where each tuple holds the name of a category and the name of the winning movie, then returns a dictionary mapping each movie to the number of the awards that it won. For example, if we provide the set:
+# { 
+#     ("Best Picture", "The Shape of Water"), 
+#     ("Best Actor", "Darkest Hour"),
+#     ("Best Actress", "Three Billboards Outside Ebbing, Missouri"),
+#     ("Best Director", "The Shape of Water"),
+#     ("Best Supporting Actor", "Three Billboards Outside Ebbing, Missouri"),
+#     ("Best Supporting Actress", "I, Tonya"),
+#     ("Best Original Score", "The Shape of Water")
+# }
+# the function should return as follows
+# { 
+#     "Darkest Hour" : 1,
+#     "Three Billboards Outside Ebbing, Missouri" : 2,
+#     "The Shape of Water" : 3,
+#     "I, Tonya" : 1
+# }
+
+def movieAwards(oscarResults):
+    # Your code goes here...
+    pass
