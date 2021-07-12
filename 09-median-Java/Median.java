3d431ab63deb9aa8a52d4cb249a2cebf82c4d783
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:00:46 +0530

    Initialize 09-median-Java

---
+++ 09-median-Java/Median.java
@@ -0,0 +1,11 @@
+// median(double[] arr) (10 pts)
+// Write the non-destructive function median(a) that takes a array of ints or floats and returns the median value, 
+// which is the value of the middle element, or the average of the two middle elements if there is no single middle 
+// element. If the given array is empty, return 0.
+
+public class Median {
+	public int median(double[] arr) {
+		// Your code goes here
+		return 0;
+	}
+}
