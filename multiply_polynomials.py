862b71b927b0f09bb23b3011ef3a33a80bc2d8a8
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:30 +0530

    Initialize multiplyPolynomials_python

---
+++ multiply_polynomials.py
@@ -0,0 +1,14 @@
+# Background: we can represent a polynomial as a list of its coefficients. 
+# For example, [2, 3, 0, 4] could represent the polynomial 2x3 + 3x2 + 4. 
+# Write the function multiplyPolynomials(p1, p2) which takes two polynomials 
+# and returns a third polynomial which is the product of the two. For example,
+# multiplyPolynomials([2,0,3], [4,5]) represents the problem (2x**2 + 3)(4x + 5), and:
+# (2x**22 + 3)(4x + 5) = 8x**3 + 10x**2 + 12x + 15
+# And so this returns [8, 10, 12, 15].
+
+def multiplyPolynomials(p1, p2):
+    # Your code goes here...
+    pass
+
+# Write your own test cases
+print ("All test cases passwed...")
