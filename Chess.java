1163ba782e59d94f8c2fa5e5dad138bfe085c857
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:07 +0530

    Initialize nQueensChecker_java

---
+++ Chess.java
@@ -0,0 +1,17 @@
+// nQueensChecker(a)
+// Background: The "N Queens" problem asks if we can place 
+// N queens on an NxN chessboard such that no two queens are 
+// attacking each other. For most values of N, there are many ways 
+// to solve this problem. Here, you will write the function 
+// nQueensChecker(board) that takes a 2d list of booleans 
+// where True indicates a queen is present and False indicates a 
+// blank cell, and returns True if this NxN board contains 
+// N queens all of which do not attack any others, 
+// and False otherwise.
+
+public class Chess {
+    public boolean nQueensChecker(int[][] a) {
+        // Your code goes here...
+        return false;
+    }
+}
