709e4b4d1b675def7a3fdc2530b870e90762b744
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:01:04 +0530

    Initialize nondestructiveRemoveRepeats-_Java

---
+++ List.java
@@ -0,0 +1,16 @@
+// nondestructiveRemoveRepeats(L)
+// Write the function nondestructiveRemoveRepeats(L), which takes a list L and nondestructively returns a new list in which any repeating elements in L are removed.
+// Specify the time complexity based on the solution that you have given.
+
+// Here are the sample test cases.
+// For example:
+// assert(nondestructiveRemoveRepeats([1, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 3, 5, 2, 7])
+// assert(nondestructiveRemoveRepeats([1, 5, 3, 3, 2, 1, 7, 5]) == [1, 5, 3, 2, 7])
+// assert(nondestructiveRemoveRepeats([1, 2, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 2, 3, 5, 7])
+
+public class List {
+    public int[] nonDestructiveRemoveRepeats(int[] arr) {
+        // Your code goes here....
+        return null;
+    }
+}
