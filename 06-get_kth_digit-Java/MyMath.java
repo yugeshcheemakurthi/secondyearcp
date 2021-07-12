ff57a8dfef53e06b02742defc29f0e98f69fd811
yugeshcheemakurthi <87293959+yugeshcheemakurthi@users.noreply.github.com>
2021-07-12 15:00:48 +0530

    Initialize 06-get_kth_digit-Java

---
+++ 06-get_kth_digit-Java/MyMath.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Write the function getKthDigit(n, k) that takes 
+ * a possibly-negative int n and a non-negative int k, 
+ * and returns the kth digit of n, starting from 0, counting from the right.
+ * if the kth digit is not present return 0.
+ * 
+ * @author Siva Sankar
+ * @version 1.0
+ */
+
+public class MyMath {
+	/**
+	 * This method returns the kth digit from the value n.
+	 * @param n the vlaue n
+	 * @param k the value k which is used to return kth digit from n.
+	 * @return the kth digit from n and 0 if the k is invalid.
+	 */
+	public int getKthDigit(int n, int k){
+		// Your code goes here...
+
+		return 0;
+	}
+}
