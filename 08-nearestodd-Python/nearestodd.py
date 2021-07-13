8c0f0fe3a4e81896843ebb76416802feca8d9f36
cheemakurthiyugesh <71495773+cheemakurthiyugesh@users.noreply.github.com>
2021-07-13 22:38:52 +0530

    comit1

--- 08-nearestodd-Python/nearestodd.py
+++ 08-nearestodd-Python/nearestodd.py
@@ -3,9 +3,12 @@
 # In the case of a tie, return the smaller odd value. 
 # Note that the result must be an int, so nearestOdd(13.0) is the int 13, and not the float 13.0.
 
-
-
+import math
 def fun_nearestodd(n):
-	return 0
+	n = math.ceil(n)
+	if(n%2!= 0):
+		return((n))
+	else:
+		return((n-1))
 
 
