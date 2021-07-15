e718546b1f6308d5c553f0e169540ff31e91b132
cheemakurthiyugesh <71495773+cheemakurthiyugesh@users.noreply.github.com>
2021-07-15 21:28:05 +0530

    commit

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -6,5 +6,5 @@
 
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	c=abs((0.5)*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)))
+	c=(0.5)*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))
 	return c
