5964bdcc44ef7ff6d6964e425984b36ddaa87b69
cheemakurthiyugesh <71495773+cheemakurthiyugesh@users.noreply.github.com>
2021-07-16 15:21:45 +0530

    commit1

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -3,5 +3,34 @@
 # that occurs most frequently in it, with ties going to the smaller digit.
 
 def mostfrequentdigit(n):
-	# your code goes here
-	pass
+	res=[]
+	dict={}
+	max=0
+	temp=0
+	if(n==0):
+		return 0
+	while(n>0):
+		x=n%10
+		res.append(x)
+		n=n//10
+	#print(res)
+	if(len(res)==2):
+		return res[1]
+	if(len(res)==1):
+		return res[0]
+	for i in res:#1,2,2,2,3,4,
+		a=res.count(i)
+		dict[i]=a
+		if(a>max):
+			max=a
+			temp=i
+	return temp
+
+		
+		
+	
+#print(mostfrequentdigit(1122322))
+
+
+
+	
