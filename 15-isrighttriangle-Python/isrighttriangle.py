96c5a6de684fbd81421f8893ca6c72cd4bda1cdd
cheemakurthiyugesh <71495773+cheemakurthiyugesh@users.noreply.github.com>
2021-07-16 08:34:10 +0530

    commit1

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,4 +7,17 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	a = ((x1-x2)*(x1-x2))+\
+        ((y1-y2)*(y1-y2))
+		
+	b = ((x1-x3)*(x1-x3))+\
+        ((y1-y3)*(y1-y3))
+		
+	c = ((x2-x3)*(x2-x3))+\
+        ((y2-y3)*(y2-y3))
+		
+	if ((a == (b+c) and a!= 0 and b!=0 and c!=0) or
+        (b == (a+c) and a!= 0 and b!= 0 and c!= 0)or
+        (c == (a+b) and a!= 0 and b!= 0 and c!= 0)):
+		return True
+	return False
