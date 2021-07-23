681abf8a38390487b89aeb6eed2064e30d24cdb1
cheemakurthiyugesh <71495773+cheemakurthiyugesh@users.noreply.github.com>
2021-07-23 10:54:40 +0530

    commit1

--- 05-rectangle_overlap-Python/rectangle_overlap.py
+++ 05-rectangle_overlap-Python/rectangle_overlap.py
@@ -8,6 +8,12 @@
 # head down (so we say that "up is down")
 
 def fun_rectangle_overlap(left1, top1, width1, height1, left2, top2, width2, height2):
-    return True
-
-        
+    
+    w=((left2>left1)and(left2>left1+width1))
+    x=((left1>left2)and(left1>left2+width2))
+    y=((top2>top1)and(top2>top1+height1))
+    z=((top1>top2)and(top1>top2+height2))
+    if(w or x or y or z):
+        return False
+    else:
+        return True
