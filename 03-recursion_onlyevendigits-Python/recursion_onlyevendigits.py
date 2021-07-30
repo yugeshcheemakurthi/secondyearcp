863d63a563f926e4ab56b0af2cc849ad9c4eaaca
cheemakurthiyugesh <71495773+cheemakurthiyugesh@users.noreply.github.com>
2021-07-30 12:33:42 +0530

    commit1

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -8,5 +8,24 @@
 # Also the function returns the empty list if the original list is empty. 
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
+def getEvenDigits(n,i=0,x=0):
+    if(n == 0):
+        return x
+    else:
+        rem = n%10
+        if(rem % 2 == 0):
+            x += rem*(10**(i))
+            i+=1 
+        return getEvenDigits(n//10,i,x)
+        
+def onlyEvenDigitsHelper(l,res=[]):
+    if(l == []):
+        return res
+    else:
+        res.append(getEvenDigits(l[0]))
+        return onlyEvenDigitsHelper(l[1:],res)
+        
 def fun_recursion_onlyevendigits(l): 
-		return []
+    if(l == []):
+        return []
+    return onlyEvenDigitsHelper(l,[])
