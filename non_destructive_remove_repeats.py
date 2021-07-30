cf6a8a27f47d68fe33c152603ca0ca322fa0027d
cheemakurthiyugesh <71495773+cheemakurthiyugesh@users.noreply.github.com>
2021-07-30 18:00:45 +0530

    commit

--- non_destructive_remove_repeats.py
+++ non_destructive_remove_repeats.py
@@ -1,5 +1,6 @@
 # nondestructiveRemoveRepeats(L)
-# Write the function nondestructiveRemoveRepeats(L), which takes a list L and nondestructively returns a new list in which any repeating elements in L are removed.
+# Write the function nondestructiveRemoveRepeats(L), which takes a list L
+#  and nondestructively returns a new list in which any repeating elements in L are removed.
 # Specify the time complexity based on the solution that you have given.
 
 # Here are the sample test cases.
@@ -9,8 +10,11 @@
 # assert(nondestructiveRemoveRepeats([1, 2, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 2, 3, 5, 7])
 
 def nondestructiveRemoveRepeats(L):
-    # Your code goes here...
-    pass
+    res=[]
+    for i in range(len(L)):
+        if L[i] not in res:
+            res.append(L[i])
+    return res
 
 assert(nondestructiveRemoveRepeats([1, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 3, 5, 2, 7])
 assert(nondestructiveRemoveRepeats([1, 5, 3, 3, 2, 1, 7, 5]) == [1, 5, 3, 2, 7])
