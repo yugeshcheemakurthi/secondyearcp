6606adcb2db02d2b6868df9ef692936028d95ff6
cheemakurthiyugesh <71495773+cheemakurthiyugesh@users.noreply.github.com>
2021-08-04 10:54:32 +0530

    commit

--- SayHi.py
+++ SayHi.py
@@ -4,5 +4,16 @@
 # sayHi(d1) # Hi, my name is Dot and I am 4 years old!
 # sayHi(d2) # Hi, my name is Elf and I am 3 years old!
 
-class SayHi(Object):
-    pass
+
+class SayHi(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+def sayHi(dog):
+    print(f'Hi, my name is {dog.name} and I am {dog.age} years old!')
+
+d1 = SayHi('Dot', 4)
+d2 = SayHi('Elf', 3)
+sayHi(d1) 
+sayHi(d2)
