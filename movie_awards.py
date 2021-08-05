f390ebd9ba7cae6cac1451c92d36f83f70880765
cheemakurthiyugesh <71495773+cheemakurthiyugesh@users.noreply.github.com>
2021-08-05 14:13:50 +0530

    comit

--- movie_awards.py
+++ movie_awards.py
@@ -16,6 +16,12 @@
 #     "I, Tonya" : 1
 # }
 
-def movieAwards(oscarResults):
+def movie_awards(oscarResults):
     # Your code goes here...
-    pass
+    d={}
+    for i in oscarResults:
+        if i[1] not in d:
+            d[i[1]]=1
+        else:
+            d[i[1]]+=1
+    return d
