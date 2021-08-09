2350e96c961e991f9b07e2b9748250529908a005
cheemakurthiyugesh <71495773+cheemakurthiyugesh@users.noreply.github.com>
2021-08-09 10:27:09 +0530

    commit

--- LatinSquare.py
+++ LatinSquare.py
@@ -7,6 +7,29 @@
 
 # Write your own test cases...
 
-def isLatinSquare(lst):
-    # Your code goes here...
-    pass
+def isLatinSquare(l):
+    x=[]
+    for i in range(len(l[0])):
+        if (l[0][i] not in x):
+            x.append(l[0][i])
+        else:
+            return False
+    for j in range(len(l)):
+        c=[]
+        r=[]
+        for k in range(len(l[j])):
+            if l[j][k] not in x:
+                return False
+            else:
+                if l[k][j] not in c:
+                    c.append(l[k][j])
+                else:
+                    return False
+                if l[j][k] not in r:
+                    r.append(l[j][k])
+                else:
+                    return False
+        print('row',r)
+        print('col',c)
+                
+    return True
